3804e63a98fbfb0bbcd2a20156a4ef9e4ec2dd2f
ives.vdf <ives.vdf@7dc089f2-f484-11de-9e29-7d1cd92887ef>
2009-12-29 22:49:10 +0000

    Placed classes in separate files

---
+++ trunk/src/Cell.h
@@ -0,0 +1,13 @@
+/**
+ *  @file Cell.h
+ *
+ *  @date 29-dec-2009
+ *  @author Ives van der Flaas
+ */
+
+#ifndef CELL_H_
+#define CELL_H_
+
+enum Cell { UP, DOWN, LEFT, RIGHT, HEAD, EMPTY, WALL, BONUS};
+
+#endif /* CELL_H_ */
