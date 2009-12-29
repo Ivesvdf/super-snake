701e73e9d94d04a897c3cc7f0c338b62402a7d4d
ives.vdf <ives.vdf@7dc089f2-f484-11de-9e29-7d1cd92887ef>
2009-12-29 22:49:22 +0000

    Made a game class

---
+++ src/Game.h
@@ -0,0 +1,39 @@
+/**
+ *  @file Game.h
+ *
+ *  @date 29-dec-2009
+ *  @author Ives van der Flaas
+ */
+
+#ifndef GAME_H_
+#define GAME_H_
+
+#include <curses.h>
+#include <vector>
+#include "Position.h"
+#include "Cell.h"
+
+using std::vector;
+
+class Game
+{
+public:
+	Game();
+	virtual ~Game();
+
+	void play();
+	void drawRel(Position pos, Cell what);
+	void setCell(Position pos, Cell what);
+	Cell getCell(Position pos) const;
+
+private:
+	WINDOW *mainwnd;
+	WINDOW *screen;
+
+	const int boardWidth;
+	const int boardHeight;
+
+	vector< vector<Cell> > *array;
+};
+
+#endif /* GAME_H_ */
